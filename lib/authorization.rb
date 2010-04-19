52beb5f8d69c23bb74b9e6b37e5efc999996ccd2
Giancarlo Palavicini <kastor@kasthor.com>
2010-04-18 18:40:04 -0600

    Writing templates in HAML

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -1,16 +1,16 @@
 module Sinatra
 	module Authorization
 
-	def require_admin
-		return if authorized?
-		headers 'WWW-Authenticate' => %(Basic realm="Authenticate")
-		throw(:halt, [401, "Not Authorized"])
-	end
+		def require_admin
+			return if authorized?
+			headers 'WWW-Authenticate' => %(Basic realm="Authenticate")
+			throw(:halt, [401, "Not Authorized"])
+		end
 
-	def authorized?
-		@auth ||= Rack::Auth::Basic::Request.new(request.env)
-		@auth.provided? and @auth.basic? and @auth.credentials == ['kasthor', 'ilkaz']
-	end
+		def authorized?
+			@auth ||= Rack::Auth::Basic::Request.new(request.env)
+			@auth.provided? and @auth.basic? and @auth.credentials == ['kasthor', 'ilkaz']
+		end
 
 	end
 end
